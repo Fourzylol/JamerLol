ab8d178b7b0f016adfc7947c1681986611f9ea50
Fourzy <94512377+Fourzylol@users.noreply.github.com>
2021-12-29 22:51:24 +0700

    Add files via upload

---
+++ plugins/donasi.js
@@ -0,0 +1,11 @@
+let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, fla + 'donasi', `
+┌「 *Donasi • Emoney* 」
+├ Dana:085748600473
+├ Indosat:085748600473
+└────
+`.trim(), '©JemerBot', 'Donasi', '.donasi', m)
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
